19d84185e66a1f719eafb3d9e75ae8d30c4c3a1c
code-for-coffee <jtraverjr@gmail.com>
2017-09-12 18:29:55 -0500

    spike!

---
+++ authHelper.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+
+var credentials = {
+  client: {
+    id: process.env.APPID,
+    secret: process.env.APPSECRET,
+  },
+  auth: {
+    tokenHost: 'https://login.microsoftonline.com',
+    authorizePath: 'common/oauth2/v2.0/authorize',
+    tokenPath: 'common/oauth2/v2.0/token'
+  }
+};
+
+var oauth2 = require('simple-oauth2').create(credentials);
+
+var redirectUri = 'http://localhost:8000/authorize';
+
+// The scopes the app requires
+var scopes = [ 'openid',
+               'offline_access',
+               'User.Read',
+               'Mail.Read' ];
+
+function getAuthUrl() {
+  var returnVal = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  });
+  console.log('Generated auth url: ' + returnVal);
+  return returnVal;
+}
+
+function getTokenFromCode(auth_code, callback, response) {
+  var token;
+  oauth2.authorizationCode.getToken({
+    code: auth_code,
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  }, function (error, result) {
+    if (error) {
+      console.log('Access token error: ', error.message);
+      callback(response, error, null);
+    } else {
+      token = oauth2.accessToken.create(result);
+      console.log('Token created: ', token.token);
+      callback(response, null, token);
+    }
+  });
+}
+
+function refreshAccessToken(refreshToken, callback) {
+  var tokenObj = oauth2.accessToken.create({refresh_token: refreshToken});
+  tokenObj.refresh(callback);
+}
+
+exports.refreshAccessToken = refreshAccessToken;
+exports.getTokenFromCode = getTokenFromCode;
+exports.getAuthUrl = getAuthUrl;
--- index.js
+++ index.js
@@ -1,14 +1,163 @@
 var server = require('./server');
 var router = require('./router');
+var authHelper = require('./authHelper');
+var url = require('url');
+var microsoftGraph = require("@microsoft/microsoft-graph-client");
+
 
 var handle = {};
 handle['/'] = home;
+handle['/authorize'] = authorize;
+handle['/mail'] = mail;
+
 
 server.start(router.route, handle);
 
 function home(response, request) {
   console.log('Request handler \'home\' was called.');
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('<p>Please <a href=\'#\'>sign in</a> with your Office 365 or Outlook.com account.</p>');
+  response.write('<p>Please <a href="' + authHelper.getAuthUrl() + '">sign in</a> with your Office 365 or Outlook.com account.</p>');
   response.end();
+}
+
+function authorize(response, request) {
+  console.log('Request handler \'authorize\' was called.');
+
+  // The authorization code is passed as a query parameter
+  var url_parts = url.parse(request.url, true);
+  var code = url_parts.query.code;
+  console.log('Code: ' + code);
+  authHelper.getTokenFromCode(code, tokenReceived, response);
+}
+
+function getUserEmail(token, callback) {
+  // Create a Graph client
+  var client = microsoftGraph.Client.init({
+    authProvider: (done) => {
+      // Just return the token
+      done(null, token);
+    }
+  });
+
+  // Get the Graph /Me endpoint to get user email address
+  client
+    .api('/me')
+    .get((err, res) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, res.mail);
+      }
+    });
+}
+
+function tokenReceived(response, error, token) {
+  if (error) {
+    console.log('Access token error: ', error.message);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<p>ERROR: ' + error + '</p>');
+    response.end();
+  } else {
+    getUserEmail(token.token.access_token, function(error, email){
+      if (error) {
+        console.log('getUserEmail returned an error: ' + error);
+        response.write('<p>ERROR: ' + error + '</p>');
+        response.end();
+      } else if (email) {
+        var cookies = ['node-tutorial-token=' + token.token.access_token + ';Max-Age=4000',
+                       'node-tutorial-refresh-token=' + token.token.refresh_token + ';Max-Age=4000',
+                       'node-tutorial-token-expires=' + token.token.expires_at.getTime() + ';Max-Age=4000',
+                       'node-tutorial-email=' + email + ';Max-Age=4000'];
+        response.setHeader('Set-Cookie', cookies);
+        response.writeHead(302, {'Location': 'http://localhost:8000/mail'});
+        response.end();
+      }
+    }); 
+  }
+}
+
+function getValueFromCookie(valueName, cookie) {
+  if (cookie.indexOf(valueName) !== -1) {
+    var start = cookie.indexOf(valueName) + valueName.length + 1;
+    var end = cookie.indexOf(';', start);
+    end = end === -1 ? cookie.length : end;
+    return cookie.substring(start, end);
+  }
+}
+
+function getAccessToken(request, response, callback) {
+  var expiration = new Date(parseFloat(getValueFromCookie('node-tutorial-token-expires', request.headers.cookie)));
+
+  if (expiration <= new Date()) {
+    // refresh token
+    console.log('TOKEN EXPIRED, REFRESHING');
+    var refresh_token = getValueFromCookie('node-tutorial-refresh-token', request.headers.cookie);
+    authHelper.refreshAccessToken(refresh_token, function(error, newToken){
+      if (error) {
+        callback(error, null);
+      } else if (newToken) {
+        var cookies = ['node-tutorial-token=' + newToken.token.access_token + ';Max-Age=4000',
+                       'node-tutorial-refresh-token=' + newToken.token.refresh_token + ';Max-Age=4000',
+                       'node-tutorial-token-expires=' + newToken.token.expires_at.getTime() + ';Max-Age=4000'];
+        response.setHeader('Set-Cookie', cookies);
+        callback(null, newToken.token.access_token);
+      }
+    });
+  } else {
+    // Return cached token
+    var access_token = getValueFromCookie('node-tutorial-token', request.headers.cookie);
+    callback(null, access_token);
+  }
+}
+
+function mail(response, request) {
+  getAccessToken(request, response, function(error, token) {
+    console.log('Token found in cookie: ', token);
+    var email = getValueFromCookie('node-tutorial-email', request.headers.cookie);
+    console.log('Email found in cookie: ', email);
+    if (token) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write('<div><h1>Your inbox</h1></div>');
+
+      // Create a Graph client
+      var client = microsoftGraph.Client.init({
+        authProvider: (done) => {
+          // Just return the token
+          done(null, token);
+        }
+      });
+
+      // Get the 10 newest messages
+      client
+        .api('/me/mailfolders/inbox/messages')
+        .header('X-AnchorMailbox', email)
+        .top(10)
+        .select('subject,from,receivedDateTime,isRead')
+        .orderby('receivedDateTime DESC')
+        .get((err, res) => {
+          if (err) {
+            console.log('getMessages returned an error: ' + err);
+            response.write('<p>ERROR: ' + err + '</p>');
+            response.end();
+          } else {
+            console.log('getMessages returned ' + res.value.length + ' messages.');
+            response.write('<table><tr><th>From</th><th>Subject</th><th>Received</th></tr>');
+            res.value.forEach(function(message) {
+              console.log('  Subject: ' + message.subject);
+              var from = message.from ? message.from.emailAddress.name : 'NONE';
+              response.write('<tr><td>' + from + 
+                '</td><td>' + (message.isRead ? '' : '<b>') + message.subject + (message.isRead ? '' : '</b>') +
+                '</td><td>' + message.receivedDateTime.toString() + '</td></tr>');
+            });
+
+            response.write('</table>');
+            response.end();
+          }
+        });
+    } else {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write('<p> No token found in cookie!</p>');
+      response.end();
+    }
+  });
 }
